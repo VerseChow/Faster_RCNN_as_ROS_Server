dec0327e3fc35d14974718251e94a9c63f632309
Zhen Zeng <zengzhen@umich.edu>
2018-07-17 13:48:21 -0400

    Fix: coco classes

--- scripts/demo.py
+++ scripts/demo.py
@@ -2,9 +2,18 @@ from obj_det_sys import *
 
 progress_coco_class = ('__background__', 'table', 'tide', 'downy', 'clorox', 'coke', 'cup')
 progress_pascal_class = ('__background__', 'table', 'tide', 'downy', 'clorox', 'coke', 'cup')
-coco_class = ('__background__', 'person', 'bicycle', 'car','motorcycle','airplane','bus','train', 'truck', 'boat','traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench','bird','cat','dog','horse','sheep','cow', 'elephant','bear','zebra','giraffe','hat','umbrella', 'handbag','tie','suitcase', 'frisbee','skis','snowboard','sports ball','kite', 'baseball bat','baseball glove','skateboard','surfboard','tennis racket', 'bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich', 'orange','broccoli','carrot','hot dog','pizza','donut','cake','chair','couch','potted plant', 'bed','dining table','window','tv','laptop','mouse','remote','keyboard','cell phone','microwave', 'oven', 'sink','refrigerator','blender','book','clock','vase','scissors','teddy bear','hair drier','tooth brush')
-complete_class =  ('__background__', 'chair', 'table', 'lobby_chair', 'lobby_table_small', 'lobby_table_large', 
-    'tide', 'spray_bottle_a', 'waterpot', 'sugar', 'red_bowl', 'clorox', 'shampoo', 
+# coco_class = ('__background__', 'person', 'bicycle', 'car','motorcycle','airplane','bus','train', 'truck', 'boat','traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench','bird','cat','dog','horse','sheep','cow', 'elephant','bear','zebra','giraffe','hat','umbrella', 'handbag','tie','suitcase', 'frisbee','skis','snowboard','sports ball','kite', 'baseball bat','baseball glove','skateboard','surfboard','tennis racket', 'bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich', 'orange','broccoli','carrot','hot dog','pizza','donut','cake','chair','couch','potted plant', 'bed','dining table','window','tv','laptop','mouse','remote','keyboard','cell phone','microwave', 'oven', 'sink','refrigerator','blender','book','clock','vase','scissors','teddy bear','hair drier','tooth brush')
+coco_class = ('__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+          'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+          'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase',
+          'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+          'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana',
+          'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+          'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+          'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+          'hair drier', 'toothbrush')
+complete_class =  ('__background__', 'chair', 'table', 'lobby_chair', 'lobby_table_small', 'lobby_table_large',
+    'tide', 'spray_bottle_a', 'waterpot', 'sugar', 'red_bowl', 'clorox', 'shampoo',
     'downy', 'salt', 'toy', 'detergent', 'scotch_brite', 'blue_cup', 'ranch')
 iros_coco_class = ('__background__', 'apple', 'bowl', 'cereal', 'coke', 'cup', 'milk', 'pringle')
 progressiros_class = ('__background__',
@@ -62,7 +71,7 @@ def object_detector(args):
 
     obj_detsys = obj_detection_system(net, gpu_flag = (not args.cpu_mode), gpu_device = args.gpu_id)
     print '\n\nLoaded network {:s}'.format(caffemodel)
-    
+
     return obj_detsys
 
 
@@ -70,9 +79,9 @@ if __name__ == '__main__':
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
     args = parse_args()
-    
+
     obj_detsys = object_detector(args)
-    
+
     # Warmup on a dummy image
     '''
     im = 128 * np.ones((224, 224, 3), dtype=np.uint8)
@@ -85,24 +94,24 @@ if __name__ == '__main__':
     if not os.path.exists('./results'):
         os.makedirs('./results')
 
-    cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE) 
-   
+    cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+
     for im_path in im_names:
         im_name = os.path.basename(im_path)
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         print 'Demo for data/demo/{}'.format(im_name)
         im = cv2.imread(im_path)
 
-          
+
         img = obj_detsys.demo(im_name)
         print img
         cv2.imshow('image',img)
         cv2.waitKey(25)
     '''
-    
+
     #plt.show()
-    
 
 
 
-    
+
+
